91942bb576e0eb27483feda64027454cdb0a6d2e
Zhang Jizhe <szanezhang@gmail.com>
2016-06-01 10:11:08 +0800

    a demo for test order-match arithmetic

--- bl/IoMatch.js
+++ bl/IoMatch.js
@@ -5,6 +5,7 @@
 var grade = require('../dao/Grade.js');
 var km = require('../dao/KmArithmetic.js');
 var Seq = require('seq');
+var fs = require('fs');
 
 function getIoMatchResult(req, res, next) {
     var orders = {};
@@ -37,25 +38,45 @@ function getIoMatchResult(req, res, next) {
                     for (var i = 0; i < ny; i++) {
                         if (km.match[i] > -1) {
                             console.log(iOrders[i].id + "," + eOrders[km.match[i]].id + "," + weight[km.match[i]][i]);
-                            map.push([iOrders[i].id, eOrders[km.match[i]].id, weight[km.match[i]][i]]);
+                            map.push([iOrders[i], eOrders[km.match[i]], weight[km.match[i]][i]]);
                         }
                     }
                 } else {
                     for (i = 0; i < ny; i++) {
                         if (km.match[i] > -1) {
                             console.log(iOrders[km.match[i]].id + "," + eOrders[i].id + "," + weight[km.match[i]][i]);
-                            map.push([iOrders[km.match[i]].id, eOrders[i].id, weight[km.match[i]][i]]);
+                            map.push([iOrders[km.match[i]], eOrders[i], weight[km.match[i]][i]]);
                         }
                     }
                 }
                 var result = {
                     sum: sum,
                     map: map
-                }
+                };
+                var data = "进口单编号,起始地,目的地,起始时间,到达时间,出口单编号,起始地,目的地,起始时间,到达时间,匹配指数\n";
+                fs.writeFile("d:\\match_result.csv", data, function (err) {
+                    if (err)
+                        console.log("fail " + err);
+                    else
+                        console.log("write done");
+                });
+                map.forEach(function (line) {
+                    data = "";
+                    data += line[0].id + "," + line[0].s_zipcode + "," + line[0].e_zipcode + "," + line[0].s_datetime + "," + line[0].e_datetime + ",";
+                    data += line[1].id + "," + line[1].s_zipcode + "," + line[1].e_zipcode + "," + line[1].s_datetime + "," + line[1].e_datetime + ",";
+                    data += line[2] + "\n";
+                    fs.appendFile("d:\\match_result.csv", data, function (err) {
+                        if (err)
+                            console.log("fail " + err);
+                        else
+                            console.log("write done");
+                    });
+                });
+                res.send(result);
+                return next();
+
             }
-            res.send(result);
-            return next();
-        })
+        });
     });
 }
 module.exports = {
